8f9324a96cc69fc5075d962eed18249740ce666e
nayanajithilanga-tech <nayanajithilanga@gmail.com>
2026-04-13 20:05:05 +0530

    server.js

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
